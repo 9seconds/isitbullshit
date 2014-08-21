ff0cf7f24aad9112213cc5bbeca18464a713009c
9seconds <nineseconds@yandex.ru>
2014-08-21 07:32:06 +0400

    Updates for setup.py

--- setup.py
+++ setup.py
@@ -2,36 +2,69 @@
 # -*- coding: utf-8 -*-
 
 
-# #############################################################################
+from setuptools import setup, find_packages
+from setuptools.command.test import test
 
 
-from setuptools import setup
+REQUIREMENTS = (
+    "six",
+)
 
 
-# #############################################################################
+with open("README.rst", "r") as resource:
+    LONG_DESCRIPTION = resource.read()
+
+
+# copypasted from http://pytest.org/latest/goodpractises.html
+class PyTest(test):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pytest_args = None  # pylint: disable=W0201
+
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []  # pylint: disable=W0201
+        self.test_suite = True  # pylint: disable=W0201
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        import sys
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
-    name='isitbullshit',
-    version='0.2.0',
-    url='https://github.com/9seconds/isitbullshit/',
-    license='MIT',
-    author='Sergey Arkhipov',
-    author_email='serge@aerialsounds.org',
-    description='Library to help with matching between JSON and simple schema',
-    long_description=__doc__,
-    py_modules=['isitbullshit'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    install_requires=[],
+    name="isitbullshit",
+    description="Small library for verifying parsed JSONs if they are bullshit or not",
+    long_description=LONG_DESCRIPTION,
+    version="0.2.0",
+    author="Sergey Arkhipov",
+    license="MIT",
+    author_email="serge@aerialsounds.org",
+    maintainer="Sergey Arkhipov",
+    maintainer_email="serge@aerialsounds.org",
+    url="https://github.com/9seconds/isitbullshit/",
+    install_requires=REQUIREMENTS,
+    tests_require=["pytest==2.6.1"],
+    packages=find_packages(),
+    cmdclass={'test': PyTest},
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Testing',
-    ]
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+
+    ],
+    zip_safe=True
 )
